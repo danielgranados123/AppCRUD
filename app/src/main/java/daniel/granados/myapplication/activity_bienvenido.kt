ac250c8d65491d67500080acb7448a6327f5379f
Daniel Granados <20200008@ricaldone.edu.sv>
2024-06-03 22:08:46 -0600

    Conexion de boton ingresar de la interfaz de bienvenida con la activity de login

--- app/src/main/java/daniel/granados/myapplication/activity_bienvenido.kt
+++ app/src/main/java/daniel/granados/myapplication/activity_bienvenido.kt
@@ -1,6 +1,8 @@
 package daniel.granados.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,13 @@ class activity_bienvenido : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Boton para ir al login
+        val btnIngresar = findViewById<Button>(R.id.btnIngresar)
+        btnIngresar.setOnClickListener {
+            val intent = Intent(this, activity_login::class.java)
+            startActivity(intent)
+        }
+
     }
 }
