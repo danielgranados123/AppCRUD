a4fc481911e6a27f3c48d86c502b64bec20cd80a
Daniel Granados <20200008@ricaldone.edu.sv>
2024-06-04 19:24:51 -0600

    Corregi tamaños en interfaces ya hechas

--- app/src/main/java/daniel/granados/myapplication/activity_login.kt
+++ app/src/main/java/daniel/granados/myapplication/activity_login.kt
@@ -1,6 +1,9 @@
 package daniel.granados.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,18 @@ class activity_login : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val imgRegresar = findViewById<ImageView>(R.id.imgRegresar)
+        imgRegresar.setOnClickListener {
+            val intent = Intent(this, activity_bienvenido::class.java)
+            startActivity(intent)
+        }
+
+        val txtRegistrateLogin = findViewById<TextView>(R.id.txtRegistrateLogin)
+        txtRegistrateLogin.setOnClickListener {
+            val intent = Intent(this, activity_registrarse::class.java)
+            startActivity(intent)
         }
     }
 }
