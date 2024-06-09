38e1bf245dcb581c6449e31b7465761b0278d29b
Daniel Granados <20200008@ricaldone.edu.sv>
2024-06-08 22:43:25 -0600

    Gracias por su ayuda profe!

--- baseDeDatos/BDAppCRUD1.sql
+++ baseDeDatos/BDAppCRUD1.sql
@@ -6,15 +6,15 @@ clave VARCHAR2(50)
 
 create table tbTickets(
 numTicket VARCHAR2(50),
-titulo varchar2(50),
-descripcion varchar2(200),
-autor varchar2(20),
-emailAutor varchar2(20),
+titulo varchar2(100),
+descripcion varchar2(300),
+autor varchar2(50),
+emailAutor varchar2(50),
 fechaCreacion varchar2(50),
 estado varchar2(10),
 fechaFinalizacion varchar2(50)
 );
 
-select * from tbUsuarios
+select * from tbUsuarios;
 
-select * from tbTickets
+select * from tbTickets;
